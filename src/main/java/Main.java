de5226543dfc4bbac844f080156d6cdc6dc897f2
Akhmad Fauzi <akhmadfauzi9797@gmail.com>
2022-08-31 20:27:39 +0700

    variable in java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 /**
  * Untuk penamaan class di java harus sama dengan nama file
  */
@@ -8,17 +10,35 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
+        /**
+         * Deklarasi variable
+         */
+        String name = "Akhmad Fauzi";
+        Integer age = 25;
+        Boolean isStatus = false;
+        Double height = 165.0;
+        Character initialsName = 'A';
 
-        // Output di java
-        System.out.println("Belajar Java");
-        System.out.println("Good luck");
-        System.out.println(3 + 3);
-        System.out.println(true);
+        Integer number1 = 25;
+        Integer number2 = 25;
+        Integer result = number1 + number2;
 
-        // print, tidak memuat baris baru
-        System.out.print(true);
-        System.out.print(false);
+        // Mencetak variable
+        System.out.println("initial name: " + initialsName);
+        System.out.println("name: " + name);
+        System.out.println("age: " + age);
+        System.out.println("Status: " + isStatus);
+        System.out.println("height: " + height);
+        System.out.println(result);
 
+        // menetapkan nilai baru
+        age = 15;
+        System.out.println("nilai age baru: " + age);
+
+//        // Keyword final adalah nilai untuk dijadikan konstan
+//        final Integer myNum = 100;
+//        // jika nilai konstant di tetapkan ke nilai baru maka kompile error
+//        myNum = 125;
     }
 
 }
