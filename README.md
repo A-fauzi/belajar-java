ea1649f357784ecf874789d2bcbd3123cf8b3e16
Akhmad Fauzi <akhmadfauzi9797@gmail.com>
2022-08-31 21:59:50 +0700

    Tipe non primitif

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 ## Tipe data dibagi menjadi 2 kelompok
 > - Tipe data primitif termasuk: byte, short, int, long, float, double, booleandanchar
 > - Tipe data non primitif termasuk: seperti String, Array dan Class
+
+
+## Tipe Data Non Primitif
+> Tipe data non-primitif disebut tipe referensi karena merujuk pada objek.
+> 
+> perbedaan utama antara tipe data primitif dan non-primitif adalah:
+> - Tipe primitif sudah ditentukan sebelumnya (sudah ditentukan) di Jawa. Tipe non-primitif dibuat oleh programmer dan tidak didefinisikan oleh Java (kecuali untuk String).
+> - Tipe non-primitif dapat digunakan untuk memanggil metode untuk melakukan operasi tertentu, sedangkan tipe primitif tidak bisa.
+> - Tipe primitif selalu memiliki nilai, sedangkan tipe non-primitif dapat berupa null.
+> - Tipe primitif dimulai dengan huruf kecil, sedangkan tipe non-primitif dimulai dengan huruf besar.
+> - Ukuran tipe primitif tergantung pada tipe data, sedangkan tipe non-primitif memiliki ukuran yang sama.
+> 
+>   Contoh tipe non-primitif adalah Strings , Arrays , Classes , Interface , dll. Anda akan mempelajari lebih lanjut tentang ini di bab selanjutnya.
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,27 +10,7 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        // TIPE DATA CHARACTER
-        // Tipe chardata digunakan untuk menyimpan satu karakter.
-        // Karakter harus diapit oleh tanda kutip tunggal, seperti 'A' atau 'c':
 
-        char myGrade = 'B';
-
-        // Atau, jika Anda terbiasa dengan nilai ASCII, Anda dapat menggunakannya untuk menampilkan karakter tertentu:
-        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
-        System.out.println(myVar1);
-        System.out.println(myVar2);
-        System.out.println(myVar3);
-
-        // String
-        // Tipe Stringdata digunakan untuk menyimpan urutan karakter (teks). Nilai string harus diapit oleh tanda kutip ganda:
-        String greeting = "Hello Java";
-        System.out.println(greeting);
-
-        // Tipe String begitu banyak digunakan dan terintegrasi di Java, sehingga beberapa orang menyebutnya " tipe kesembilan khusus ".
-        //
-        // Sebuah String di Jawa sebenarnya adalah tipe data non-primitif ,
-        // karena mengacu pada sebuah objek. Objek String memiliki metode yang digunakan untuk melakukan operasi tertentu pada string.
     }
 
 }
