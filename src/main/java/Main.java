06413f03eb66cbe8465e959efd41aa88f773ee13
Akhmad Fauzi <akhmadfauzi9797@gmail.com>
2022-08-31 19:47:31 +0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Untuk penamaan class di java harus sama dengan nama file
+ */
+public class Main {
+
+    /**
+     * fungsi main adalah fungsi utama untuk menjalankan program di Java
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+
+}
