1a30b290c6dca494882824f789ea99bc0ac153f3
Akhmad Fauzi <akhmadfauzi9797@gmail.com>
2022-09-01 11:18:01 +0700

    Switch steatment

---
+++ README.md
@@ -0,0 +1,13 @@
+## Java Switch Statements
+> - switch Ekspresi dievaluasi sekali.
+> - Nilai ekspresi dibandingkan dengan nilai masing-masing case.
+> - Jika ada kecocokan, blok kode yang terkait akan dieksekusi.
+
+## Break keyword
+> Jika mencapai kata kunci break, makan eksekusi akan di hentikan
+> 
+> Ketika ada kondisi yang cocok, makan akan di hentikan oleh break keyword,
+> dan tidak ada pengujian lanjutan
+
+## Default keyword
+> Kata default kata kunci menentukan beberapa kode untuk dijalankan jika tidak ada kasus yang cocok:
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,43 +11,38 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        // If Else Condition
-        // Anda dapat menggunakan kondisi ini untuk melakukan tindakan yang berbeda untuk keputusan yang berbeda.
-        //
-        // Java memiliki pernyataan bersyarat berikut:
-        /*
-            Gunakan if untuk menentukan blok kode yang akan dieksekusi, jika kondisi yang ditentukan benar
-            Gunakan else untuk menentukan blok kode yang akan dieksekusi, jika kondisi yang sama salah
-            Gunakan else if untuk menentukan kondisi baru yang akan diuji, jika kondisi pertama salah
-            Gunakan switch untuk menentukan banyak blok kode alternatif yang akan dieksekusi
+        String day = "ddd";
 
-         */
+        // Java Switch Statements
+        // Gunakan switchpernyataan untuk memilih salah satu dari banyak blok kode yang akan dieksekusi.
+        switch (day.toLowerCase()) {
+            case "senin":
+                System.out.println("Kerja");
+                break;
+            case "selasa":
+                System.out.println("Kerja");
+                break;
+            case "rabu":
+                System.out.println("Kerja");
+                break;
+            case "kamis":
+                System.out.println("Kerja");
+                break;
+            case "jumat":
+                System.out.println("Kerja");
+                break;
+            case "sabtu":
+                System.out.println("Libur");
+                break;
+            case "minggu":
+                System.out.println("Libur");
+                break;
 
-        boolean statusNikah = true;
-        if (statusNikah) {
-            System.out.println("Sudah Menikah");
-        } else {
-            System.out.println("Belum menikah");
+                // default akan dijalankan jika kondisi tidak ada kecocokan
+            default:
+                System.out.println("Hari tidak ada");
+
         }
-
-        int bangunTidur = 18;
-        if (bangunTidur < 10) {
-            System.out.println("Good Morning");
-        } else if (bangunTidur < 17) {
-            System.out.println("Good day");
-        } else {
-            System.out.println("Good night");
-        }
-
-
-        // Ternary Operator
-
-        // Short Hand If...Else
-        // Ada juga short-hand if else , yang dikenal sebagai operator ternary karena terdiri dari tiga operan.
-        // Ini dapat digunakan untuk mengganti beberapa baris kode dengan satu baris, dan paling sering digunakan untuk mengganti pernyataan if else sederhana:
-        int age = 14;
-        String checkAge = (age < 17) ? "Maaf belum cukup umur" : "Sudah Cukup Umur, silahkan masuk";
-        System.out.println(checkAge); // maaf
     }
 
 }
