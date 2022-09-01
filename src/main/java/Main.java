de05d9a595bedb59ee422126586b52be3a2b0973
Akhmad Fauzi <akhmadfauzi9797@gmail.com>
2022-09-01 11:56:32 +0700

    For loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,28 +11,30 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        // Loop
-        // Loop dapat mengeksekusi blok kode selama kondisi tertentu tercapai.
-        // Loop berguna karena menghemat waktu, mengurangi kesalahan, dan membuat kode lebih mudah dibaca.
+        // For Loop
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        /*
+            Pernyataan 1 menetapkan variabel sebelum loop dimulai (int i = 0).
 
-        // Java while loop
-        // Loop whileloop melalui blok kode selama kondisi yang ditentukan adalah true:
+            Pernyataan 2 mendefinisikan kondisi untuk menjalankan loop (i harus kurang dari 5). Jika kondisi benar, loop akan dimulai lagi, jika salah, loop akan berakhir.
 
-        int num = 0;
-        while (num < 10) {
-            System.out.println(num);
-            num++;
+            Pernyataan 3 meningkatkan nilai (i++) setiap kali blok kode dalam loop telah dieksekusi.
+         */
+
+        // Contoh ini hanya akan mencetak nilai genap antara 0 dan 10:
+        for (int i = 0; i <= 10; i += 2){
+            System.out.println(i);
         }
 
-        // Do-While
-        // Loop do/whileadalah varian dari whileloop.
-        // Loop ini akan mengeksekusi blok kode satu kali, sebelum memeriksa apakah kondisinya benar, kemudian akan mengulangi loop selama kondisinya benar.
-        // Loop akan selalu dieksekusi setidaknya sekali, bahkan jika kondisinya salah, karena blok kode dieksekusi sebelum kondisi diuji:
-        int i = 0;
-        do {
-            System.out.println("cetak" + " " + i);
-            i++;
-        } while (i < 10);
+        // Java Foreach
+        // Ada juga loop " for-each ", yang digunakan secara eksklusif untuk mengulang elemen dalam array :
+        String[] car = {"car 1", "car 2", "car 3"};
+        for (String i : car) {
+            System.out.println(i);
+        }
+
     }
 
 }
