52d0ec3e382b3f7163e75649581f229ec5a18fe8
Akhmad Fauzi <akhmadfauzi9797@gmail.com>
2022-09-01 16:27:37 +0700

    attribute class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,34 +4,55 @@ import java.awt.*;
  * Untuk penamaan class di java harus sama dengan nama file
  */
 public class Main {
-    // variable di dalam class
+
+    // Attribute class di java
+
+    // attribute class
+    // istilah lain dari attribute adalah field
     int x = 10;
+    final int y = 10;
 
-    /**
-     * fungsi main adalah fungsi utama untuk menjalankan program di Java
-     *
-     * @param args
-     */
+    // Menambahkan beberapa attribute
+    String fName = "Akhmad";
+    String lName = "Fauzi";
+    int age = 25;
+    String email = "akhmadfauzi@gmail.com";
+
     public static void main(String[] args) {
-        // Java Class & Object
-        // Java adalah bahasa pemrograman berorientasi objek.
-        // Segala sesuatu di Java dikaitkan dengan kelas dan objek, bersama dengan atribut dan metodenya. Sebagai contoh: dalam kehidupan nyata, mobil adalah sebuah objek.
-        // Mobil memiliki atribut , seperti berat dan warna, dan metode , seperti drive dan rem.
-        // Class seperti konstruktor objek, atau "blue print" untuk membuat objek.
+        // mengakses attribute
+        Main objX = new Main();
+        Main objY = new Main();
+        System.out.println(objX.x);
+        System.out.println(objY.y);
 
-        // Buat Object
-        // Di java, sebuah object di buat dari sebuah class
-        // class Main dapat digunakan untuk membuat Object
-        // Untuk membuat object Main, tentukan nama kelas, diikuti dengan nama object dan gunakan kata kunci new
+        // Ubah nilai pada attribute
+        int x = objX.x = 1;
+//        int y = objY.y = 1; // not assigment, karna attribute tersebut adalah final
+        System.out.println(x);
+//        System.out.println(y);
 
-        Main myObj = new Main();
-        // Mendapatkan sebuah object property pada class
-        System.out.println(myObj.x);
+        // cara merubah attribute yang final
 
-        // Beberapa object
-        Main myObj1 = new Main();
-        Main myObj2 = new Main();
-        System.out.println(myObj1.x + myObj2.x);
+//        objY.y = 20; // not assigment
+        int changeFinalAttribute = objY.y;
+        changeFinalAttribute = 20;
+        System.out.println(changeFinalAttribute);
+
+        // Beberapa Object
+        Main obj1 = new Main();
+        Main obj2 = new Main();
+        Main obj3 = new Main();
+        System.out.println(obj1.x);
+        System.out.println(obj2.x);
+        System.out.println(obj3.x);
+
+        // Beberapa attribute
+        Main obj = new Main();
+        System.out.println("first name: " + obj.fName);
+        System.out.println("last name: " + obj.lName);
+        System.out.println("age: " + obj.age);
+        System.out.println("email: " + obj.email);
     }
+
 
 }
