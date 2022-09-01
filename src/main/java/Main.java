0f4afc3997b690e15e21f6f7a233889f4e75746b
Akhmad Fauzi <akhmadfauzi9797@gmail.com>
2022-09-01 11:23:13 +0700

    if else

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,36 +13,65 @@ public class Main {
     public static void main(String[] args) {
         String day = "ddd";
 
-        // Java Switch Statements
-        // Gunakan switchpernyataan untuk memilih salah satu dari banyak blok kode yang akan dieksekusi.
-        switch (day.toLowerCase()) {
-            case "senin":
-                System.out.println("Kerja");
-                break;
-            case "selasa":
-                System.out.println("Kerja");
-                break;
-            case "rabu":
-                System.out.println("Kerja");
-                break;
-            case "kamis":
-                System.out.println("Kerja");
-                break;
-            case "jumat":
-                System.out.println("Kerja");
-                break;
-            case "sabtu":
-                System.out.println("Libur");
-                break;
-            case "minggu":
-                System.out.println("Libur");
-                break;
+        // Java Boolean
+        // Sangat sering, dalam pemrograman, Anda memerlukan tipe data yang hanya dapat memiliki satu dari dua nilai, seperti:
+        // YA TIDAK
+        // AKTIF / MATI
+        // BENAR SALAH
+        // Untuk ini, Java memiliki booleantipe data, yang dapat mengambil nilai trueatau false.
 
-                // default akan dijalankan jika kondisi tidak ada kecocokan
-            default:
-                System.out.println("Hari tidak ada");
+        // Boolean values
+        // Tipe boolean dideklarasikan dengan booleankata kunci dan hanya dapat mengambil nilai trueatau false:
+        boolean javaIsFun = true;
+        boolean jsIsFun = false;
+        System.out.println(javaIsFun);
+        System.out.println(jsIsFun);
 
+        // Boolean expression
+        int y = 10;
+        int x = 15;
+        boolean check = y > x;
+        System.out.println(check); // false
+        System.out.println(20 < 25); // true
+        System.out.println(20 == 20); // true
+        System.out.println(20 == 22); // false
+        // If Else Condition
+        // Anda dapat menggunakan kondisi ini untuk melakukan tindakan yang berbeda untuk keputusan yang berbeda.
+        //
+        // Java memiliki pernyataan bersyarat berikut:
+        /*
+            Gunakan if untuk menentukan blok kode yang akan dieksekusi, jika kondisi yang ditentukan benar
+            Gunakan else untuk menentukan blok kode yang akan dieksekusi, jika kondisi yang sama salah
+            Gunakan else if untuk menentukan kondisi baru yang akan diuji, jika kondisi pertama salah
+            Gunakan switch untuk menentukan banyak blok kode alternatif yang akan dieksekusi
+         */
+
+        boolean statusNikah = true;
+        if (statusNikah) {
+            System.out.println("Sudah Menikah");
+        } else {
+            System.out.println("Belum menikah");
         }
-    }
 
+        int bangunTidur = 18;
+        if (bangunTidur < 10) {
+            System.out.println("Good Morning");
+        } else if (bangunTidur < 17) {
+            System.out.println("Good day");
+        } else {
+            System.out.println("Good night");
+        }
+
+
+        // Ternary Operator
+
+        // Short Hand If...Else
+        // Ada juga short-hand if else , yang dikenal sebagai operator ternary karena terdiri dari tiga operan.
+        // Ini dapat digunakan untuk mengganti beberapa baris kode dengan satu baris, dan paling sering digunakan untuk mengganti pernyataan if else sederhana:
+        int age = 14;
+        String checkAge = (age < 17) ? "Maaf belum cukup umur" : "Sudah Cukup Umur, silahkan masuk";
+        System.out.println(checkAge); // maaf
+
+    }
 }
+
