a036c44b3d9a20c5e251e1c21e95a745c024f162
Akhmad Fauzi <akhmadfauzi9797@gmail.com>
2022-09-06 13:36:54 +0700

    getter setter encapsulation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,45 +4,10 @@ import java.awt.*;
  * Untuk penamaan class di java harus sama dengan nama file
  */
 public class Main {
-    // Final
-    // Jika tidak ingin merubah nilai pada attribute, nyatakan attribute tersebut menjadi final
-    final String gender = "Laki-Laki";
-    final String nik = "3275022601970006";
-
     // main method
     public static void main(String[] args) {
-        Main person = new Main();
-//        person.gender = ""  -> error not assign final attribute
-//        person.nik = "6234642"; -> error not assign final attribute
-
-        // Call static methode
-        myStaticVoid();
-
-        // call public method
-        Main obj = new Main();
-        obj.myPublicVoid();
-
-        // Call student class
-        Student student = new Student();
-        String name = student.name;
-        String age = String.valueOf(student.age);
-
-        System.out.println(name);
-        System.out.println(age);
-        student.study();
-
-    }
-
-    // Static
-    // method static dapat di akses tanpa membuat object class, tidak seperti public
-
-    // Static method
-    static void myStaticVoid() {
-        System.out.println("Static method execute");
-    }
-
-    // Public method
-    public void myPublicVoid() {
-        System.out.println("Public method execute");
+        Person obj = new Person();
+        obj.setName("Tikus");
+        System.out.println(obj.getName());
     }
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -2,9 +2,17 @@
 // Sebuah abstract metode milik abstract kelas, dan tidak memiliki tubuh. Tubuh disediakan oleh subclass:
 
 // abstract class
-abstract public class Person {
-    // abstract property
-    public String name = "Akhmad Fauzi";
-    public int age = 25 ;
-    public abstract void study(); // abstract method
+public class Person {
+    private String name; // private = restricted access
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    // Setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
